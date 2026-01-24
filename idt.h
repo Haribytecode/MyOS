afcdd1f12d5568d374d2566114c5f7f8d65b6c8e
Hariharan <osdevhari@gmail.com>
2026-01-24 10:14:54 +0530

    Update idt.h

--- idt.h
+++ idt.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-/* * This function sets up the IDT structure, 
- * fills it with default handlers, and loads 
- * the pointer into the CPU using the assembly function.
+/* * This function sets up the IDT table structure, 
+ * fills it with default handler functions, and then loads 
+ * pointer into CPU using assembly function.
  */
 void idt_init(void);
 
