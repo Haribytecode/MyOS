30179f5d9c24824301a11cc636e6cdcb5f0156c9
Hariharan <osdevhari@gmail.com>
2026-01-24 10:36:31 +0530

    Update uart.c

--- uart.c
+++ uart.c
@@ -2,26 +2,25 @@
 #include "uart.h"
 #include "sync.h"
 
-// We use the COM1 port address
+
 #define PORT 0x3F8
 
-// Helper to send data to hardware
+
 static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
 void uart_init() {
-    outb(PORT + 1, 0x00);    // Disable all interrupts
-    outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-    outb(PORT + 0, 0x03);    // Set divisor to 3 (38400 baud)
-    outb(PORT + 1, 0x00);    // (hi byte)
-    outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-    outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+    outb(PORT + 1, 0x00);    
+    outb(PORT + 3, 0x80);    
+    outb(PORT + 0, 0x03);    
+    outb(PORT + 1, 0x00);   
+    outb(PORT + 3, 0x03);    
+    outb(PORT + 2, 0xC7);    
 }
 
 void uart_putc(char c) {
-    // We should technically check if the transmit holding register is empty, 
-    // but for simple debug, we just blast it out.
+ 
     outb(PORT, c);
 }
 
