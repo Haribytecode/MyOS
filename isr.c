0b3f4bdbcdc7b45435d4ac6fea3b3ed3c7ed322c
Hariharan <osdevhari@gmail.com>
2026-01-24 10:16:54 +0530

    Update isr.c

--- isr.c
+++ isr.c
@@ -1,12 +1,11 @@
 #include "panic.h"
 #include "keyboard.h"
 
-// Declaration of our handler
 extern void timer_handler(void);
 
 void isr_handler(void) {
-    // For Day 21, since we don't have "Interrupt Numbers" yet,
-    // we just call timer_handler. 
-    // BUT: Comment out the divide-by-zero in kernel.c for now!
+
+    //calling timer_handler function 
+   
     timer_handler();
 }
