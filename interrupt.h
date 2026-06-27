adcfb4fdf37353c53e3e3e5b90543391b7446e4f
Haribytecode <osdevhari@gmail.com>
2026-06-27 19:21:43 +0530

    Golden baseline before Scheduler V2 rewrite

--- interrupt.h
+++ interrupt.h
@@ -3,19 +3,35 @@
 
 #include <stdint.h>
 
+/*
+ * Layout of the interrupt frame after:
+ *
+ *   CPU pushes:
+ *       EIP
+ *       CS
+ *       EFLAGS
+ *
+ *   timer_stub executes:
+ *       pusha
+ *
+ * This structure MUST exactly match the stack layout.
+ */
+
 typedef struct interrupt_frame
 {
-    /* pushad */
+    /* pusha (reverse pop order) */
+
     uint32_t edi;
     uint32_t esi;
     uint32_t ebp;
-    uint32_t esp;
+    uint32_t esp_dummy;
     uint32_t ebx;
     uint32_t edx;
     uint32_t ecx;
     uint32_t eax;
 
-    /* CPU pushed */
+    /* CPU interrupt frame */
+
     uint32_t eip;
     uint32_t cs;
     uint32_t eflags;
